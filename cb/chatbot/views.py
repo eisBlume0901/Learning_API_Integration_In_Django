4da70b1e38775d23f099e92a87e02f5e2ec83e15
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2024-09-02 17:28:01 +0800

    Initial template layout

--- cb/chatbot/views.py
+++ cb/chatbot/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def chatbot(request):
+    return render(request, 'chatbot.html')
